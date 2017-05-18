bd84e735cf60adbdebcd04eae2fdfef9ee2aea99
kevin <kevin.fuwenkai@gmail.com>
2017-05-18 15:14:13 -0500

    Add nice_plot.py and experimental schedule (expe_schedule.JPG)

---
+++ rtd/in_core_data/apr_27_17/nice_plot.py
@@ -0,0 +1,14 @@
+from matplotlib import rc
+rc('font',**{'family':'serif'})
+from matplotlib import rcParams
+rcParams['xtick.direction'] = 'out'
+rcParams['ytick.direction'] = 'out'
+rcParams['xtick.labelsize'] = 18
+rcParams['ytick.labelsize'] = 18
+rcParams['lines.linewidth'] = 1.85
+rcParams['axes.labelsize'] = 20
+rcParams['legend.numpoints'] = 1
+rcParams['lines.markersize'] = 10
+rcParams.update({'figure.autolayout': True})
+import numpy as np
+import matplotlib.pyplot as plt
