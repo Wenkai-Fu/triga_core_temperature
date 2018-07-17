ff33957bc3be794355560059fe51ba4d3e240cb7
kevin <kevin.fuwenkai@gmail.com>
2018-07-17 17:52:13 -0500

    compare to in-pool calibration

--- rtd/calibration/wi_probe/process/process.py
+++ rtd/calibration/wi_probe/process/process.py
@@ -1,44 +1,5 @@
 from nice_plot import *
 
-labjack139 = np.loadtxt('../dustin/139_cal_0.dat')
-labjack166 = np.loadtxt('../dustin/166_cal_0.dat')
-
-
-# readout of old probe b, RTD 1 to 8
-ogb1 = labjack139[:, 1 : 7]
-ogb2 = labjack166[:, -2 : ]
-
-# readout of three wi probes
-pbwi = labjack139[:, -6 : ]
-pawi = labjack166[:, 1 : 7]
-pcwi = labjack166[:, 7 : 13]
-
-# calibration equation of the 9 RTDs in old probe b
-eqb = np.loadtxt('/home/kevin/workspace/triga_core_temperature_old/rtd/calibration/summary2/probeb_kw_cal_eq.txt')
-
-# temperature measured by the original probe b, 8 RTDs
-tem = []
-# loop over  RTD 1 to 6 in original probe b
-for i in range(len(ogb1[0])):
-    rtd = ogb1[:, i]
-    aveV = np.average(rtd)
-    poly = eqb[i]
-    aT = np.polyval(poly, aveV)
-    tem.append(aT)
-# loop over RTD 7 & 8 in original probe b
-for i in range(len(ogb2[0])):
-    rtd = ogb2[:, i]
-    aveV = np.average(rtd)
-    poly = eqb[i + 6]
-    aT = np.polyval(poly, aveV)
-    tem.append(aT)
-    
-print 'measured temperature of the 8 RTDs in original probe b'
-print tem
-aveTog = np.average(tem)
-std = np.std(tem, ddof = 1)
-print 'average T measured by original probe b = %.2f +- %.2f' % (aveTog, std)
-
 # avetage voltage over time of the three new UW probes at 25.3 and 22.3C
 pa_avev = np.zeros((6, 2))
 pb_avev = np.zeros((6, 2))
@@ -66,7 +27,7 @@ pa_avev[:, 1] = np.average(pa, axis = 0)
 pb_avev[:, 1] = np.average(pb, axis = 0)
 pc_avev[:, 1] = np.average(pc, axis = 0)
 
-
+# plot average voltage vs temperature
 tem = [25.3, 22.3]
 markers = ['sb-', '*g-', 'Dr-', 'hm-', 'pc-', 'vk-']
 plt.figure()
@@ -96,7 +57,6 @@ plt.ylabel('Temperature (C)')
 plt.legend(loc = 0)
 plt.savefig('pc_uw_cal.pdf')
 
-
 # fit equations
 polyA = []
 for i in range(6):
@@ -114,7 +74,73 @@ polyC = []
 for i in range(6):
     poly = np.polyfit(pc_avev[i], tem, deg = 1)
     polyC.append(poly)
-np.savetxt('pCwi_cal_eq.txt', polyA)
+np.savetxt('pCwi_cal_eq.txt', polyC)
+
+
+# check the fit equation with Dustin's in-pool results
+labjack139 = np.loadtxt('../dustin/139_cal_0.dat')
+labjack166 = np.loadtxt('../dustin/166_cal_0.dat')
+
+
+# readout of original probe b, RTD 1 to 8
+ogb1 = labjack139[:, 1 : 7]
+ogb2 = labjack166[:, -2 : ]
+
+# calibration equation of the 9 RTDs in old probe b
+eqb = np.loadtxt('/home/kevin/workspace/triga_core_temperature_old/rtd/calibration/summary2/probeb_kw_cal_eq.txt')
+
+# temperature measured by the original probe b, 8 RTDs
+tem = []
+# loop over  RTD 1 to 6 in original probe b
+for i in range(len(ogb1[0])):
+    rtd = ogb1[:, i]
+    aveV = np.average(rtd)
+    poly = eqb[i]
+    aT = np.polyval(poly, aveV)
+    tem.append(aT)
+# loop over RTD 7 & 8 in original probe b
+for i in range(len(ogb2[0])):
+    rtd = ogb2[:, i]
+    aveV = np.average(rtd)
+    poly = eqb[i + 6]
+    aT = np.polyval(poly, aveV)
+    tem.append(aT)
+    
+print 'measured temperature of the 8 RTDs in original probe b'
+print tem
+aveTog = np.average(tem)
+std = np.std(tem, ddof = 1)
+print 'average T measured by original probe b = %.2f +- %.2f' % (aveTog, std)
+
+# readout of three wi probes
+pbwi = labjack139[:, -6 : ]
+pawi = labjack166[:, 1 : 7]
+pcwi = labjack166[:, 7 : 13]
+
+# average over time
+pa_avev = np.average(pawi, axis = 0)
+pb_avev = np.average(pbwi, axis = 0)
+pc_avev = np.average(pcwi, axis = 0)
+
+# calculate temperature by fitted equation
+ta = []
+tb = []
+tc = []
+for i in range(6):
+    tmp = np.polyval(polyA[i], pa_avev[i])
+    ta.append(tmp)
+    tmp = np.polyval(polyB[i], pb_avev[i])
+    tb.append(tmp)
+    tmp = np.polyval(polyC[i], pc_avev[i])
+    tc.append(tmp)
+print '\ncalculated T of probe A using fitted equation'
+print ta
+
+print '\ncalculated T of probe B using fitted equation'
+print tb
+
+print '\ncalculated T of probe C using fitted equation'
+print tc
 
 
 
